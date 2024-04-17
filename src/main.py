5d1f8e03060feb57d208817d92096a55bd270d81
Gustavo R. Liberado <grlgustavo@gmail.com>
2024-04-16 21:47:18 -0300

    Criação da janela básica

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
-import PySide6
+import sys
+from PySide6.QtWidgets import QApplication, QLabel
+from main_window import MainWindow
 
 if __name__ == '__main__':
-    ...
+    app = QApplication(sys.argv)
+    window = MainWindow()
+
+    lbl_1 = QLabel('O meu texto é grande')
+    lbl_1.setStyleSheet('font-size: 40px')
+    window.v_layout.addWidget(lbl_1)
+
+    window.adjustFixedSize()
+    window.show()
+    app.exec()
