90eaca8c82d0c17203d817db8511258aec8c5aa6
Gustavo R. Liberado <grlgustavo@gmail.com>
2024-04-16 23:03:43 -0300

    limpando a parte do label

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
 from main_window import MainWindow
 from variables import WINDOWS_ICO_PATH
@@ -8,9 +8,6 @@ if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
-
-    lbl_1 = QLabel('O meu texto é grande')
-    lbl_1.setStyleSheet('font-size: 40px')
 
     # Defini o ícone
     icon = QIcon(str(WINDOWS_ICO_PATH))
