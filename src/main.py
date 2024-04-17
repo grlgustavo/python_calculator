0f5f41619e3b60064417f38555ca426cfc99acb5
Gustavo R. Liberado <grlgustavo@gmail.com>
2024-04-16 23:02:05 -0300

    adicoiinando icone

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
 import sys
 from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtGui import QIcon
 from main_window import MainWindow
+from variables import WINDOWS_ICO_PATH
 
 if __name__ == '__main__':
+    # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
 
     lbl_1 = QLabel('O meu texto é grande')
     lbl_1.setStyleSheet('font-size: 40px')
-    window.v_layout.addWidget(lbl_1)
 
+    # Defini o ícone
+    icon = QIcon(str(WINDOWS_ICO_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
+    if sys.platform.startswith('win'):
+        import ctypes
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            u'CompanyName.ProductName.SubProduct.VersionInformation')
+
+    # Executa tudo
     window.adjustFixedSize()
     window.show()
     app.exec()
---
+++ src/variables.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+ROOT_FOLDER = Path(__file__).parent.parent
+IMG_DIR = ROOT_FOLDER / 'img'
+WINDOWS_ICO_PATH = IMG_DIR / 'calculator_ico.png'
